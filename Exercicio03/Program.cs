2b3f880a1f0f7657cdee7989a959380c899a90de
Tiago Correa dos Anjos <tiagocdosanjos@gmail.com>
2024-04-09 21:37:35 -0300

    Exercicio03

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio03
+{
+    internal class Program
+    {
+        //Crie um programa que preencha uma matriz 3x3 com valores inteiros informados pelo usuário e depois exiba essa matriz na tela.
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($"Digite o valor para matriz {i}, {j}: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write(matriz[i, j] + "\t ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
