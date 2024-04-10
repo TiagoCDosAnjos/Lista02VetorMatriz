0a9dd6159a6a4ae5ec8c558c0a830040192da86f
Tiago Correa dos Anjos <tiagocdosanjos@gmail.com>
2024-04-10 13:45:21 -0300

    Exercicio04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio04
+{
+    internal class Program
+    {
+        //Escreva um programa que declare dois vetores de mesmo tamanho e calcule a soma dos elementos correspondentes de ambos os vetores,
+        //armazenando o resultado em um terceiro vetor. Em seguida, exiba o vetor resultante.
+        static void Main(string[] args)
+        {
+            int[] vetor1 = { 45, 15 };
+            int[] vetor2 = { 2, 70 };
+            int[] vetorSoma = new int[2];
+
+            for (int i = 0; i < vetor1.Length; i++)
+            {
+                vetorSoma[i] = vetor1[i] + vetor2[i];
+            }
+
+            foreach (int i in vetorSoma)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
