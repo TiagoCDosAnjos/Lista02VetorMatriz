ac1a132d18ea9da04ab474fddd7ee9d18c750224
Tiago Correa dos Anjos <tiagocdosanjos@gmail.com>
2024-04-11 13:46:45 -0300

    Exercicio06

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq.Expressions;
+
+namespace Exercicio06
+{
+    internal class Program
+    {
+        //\t
+        static void Main(string[] args)
+        {
+            string[,] animais = {
+                   { "Fido", "Cachorro", "5", "10kg" },
+                   { "Whiskers", "Gato", "5", "5kg" },
+                   { "Buddy", "Cachorro", "3", "8kg" },
+                   { "Fluffy", "Gato", "2", "4kg" },
+                   { "Spot", "Cachorro", "4", "12kg" }
+            };
+
+            Console.Write("Deseja buscar por [nome], [espécie], [idade] ou [peso]: ");
+            string buscar = Console.ReadLine();
+
+            Console.Write("Digite a propiedade a ser buscada: ");
+            string propiedade = Console.ReadLine();
+
+            BuscarAnimal(animais, buscar, propiedade);
+            Console.ReadKey();
+        }
+
+        static void BuscarAnimal(string[,] matrizAnimais, string propiedade, string valor)
+        {
+            int index = IndexDaMatriz(propiedade);
+            for (int i = 0; i < matrizAnimais.GetLength(0); i++)
+            {
+                if (matrizAnimais[i, index] == valor)
+                    Console.WriteLine($"Nome: {matrizAnimais[i, 0]}, Espécie: {matrizAnimais[i, 1]}, Idade: {matrizAnimais[i, 2]}, Peso: {matrizAnimais[i, 3]}");
+            }
+
+            if (index >= 4)
+                Console.WriteLine("Animal não encontrado");
+        }
+
+        static int IndexDaMatriz(string propiedade)
+        {
+            switch (propiedade)
+            {
+                case "nome":
+                    return 0;
+                    break;
+                case "espécie":
+                    return 1;
+                    break;
+                case "idade":
+                    return 2;
+                    break;
+                case "peso":
+                    return 3;
+                    break;
+                default:
+                    return 4;
+            }
+        }
+    }
+}
--- Testes/Program.cs
+++ Testes/Program.cs
@@ -4,70 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Criando uma instância de Random para gerar números aleatórios
-        Random random = new Random();
 
-        // Criando uma matriz 4x4 de números inteiros
-        int[,] matriz = new int[4, 4];
-
-        // Preenchendo a matriz com números aleatórios
-        PreencherMatrizAleatoria(matriz, random);
-
-        // Exibindo a matriz
-        Console.WriteLine("Matriz gerada:");
-        ExibirMatriz(matriz);
-
-        // Encontrando o maior valor na matriz
-        int maiorValor = EncontrarMaiorValor(matriz);
-
-        // Exibindo o maior valor encontrado
-        Console.WriteLine("\nO maior valor na matriz é: " + maiorValor);
-
-        Console.ReadLine(); // Para manter o console aberto após a execução do programa
-    }
-
-    // Método para preencher a matriz com números aleatórios
-    static void PreencherMatrizAleatoria(int[,] matriz, Random random)
-    {
-        for (int i = 0; i < matriz.GetLength(0); i++)
-        {
-            for (int j = 0; j < matriz.GetLength(1); j++)
-            {
-                matriz[i, j] = random.Next(100); // Gerando números aleatórios de 0 a 99
-            }
-        }
-    }
-
-    // Método para exibir a matriz
-    static void ExibirMatriz(int[,] matriz)
-    {
-        for (int i = 0; i < matriz.GetLength(0); i++)
-        {
-            for (int j = 0; j < matriz.GetLength(1); j++)
-            {
-                Console.Write(matriz[i, j] + "\t");
-            }
-            Console.WriteLine();
-        }
-    }
-
-    // Método para encontrar o maior valor na matriz
-    static int EncontrarMaiorValor(int[,] matriz)
-    {
-        int maiorValor = matriz[0, 0]; // Inicializando com o primeiro valor da matriz
-
-        for (int i = 0; i < matriz.GetLength(0); i++)
-        {
-            for (int j = 0; j < matriz.GetLength(1); j++)
-            {
-                if (matriz[i, j] > maiorValor)
-                {
-                    maiorValor = matriz[i, j];
-                }
-            }
-        }
-
-        return maiorValor;
     }
 }
+
 
